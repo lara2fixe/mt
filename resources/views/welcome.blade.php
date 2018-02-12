86804bf127bdc84c9e07bec62611e4bfe04b5155
lara2fixe <fixe@mail.ru>
2018-02-12 13:41:29 +0300

    Изменен шаблон

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravella
                 </div>
 
                 <div class="links">
